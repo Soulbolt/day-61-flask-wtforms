62ec1e8b29515e9a822b7c03370f61af214a34a4
Soulbolt <soulbolt@gmail.com>
2024-01-12 13:12:47 -0700

    added statement to check on login form validation to match admin credentials and route to success or denied html respectavely.

--- main.py
+++ main.py
@@ -32,7 +32,11 @@ def home():
 @app.route("/login", methods={"GET", "POST"})
 def login():
     login_form = MyLoginForm()  #  create variable to passon login form to html
-    login_form.validate_on_submit() # Validates form is properly filled
+    if login_form.validate_on_submit(): # Validates form is properly filled with correct credentials
+        if (login_form.email.data == "admin@email.com") & (login_form.password.data == "12345678"):
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
     return render_template("login.html", form=login_form)
 
 
